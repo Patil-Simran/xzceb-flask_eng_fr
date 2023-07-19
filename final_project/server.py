e5126bfe4ab1c741bf3b0a28bced0407e17d446a
Patil-Simran <simranpatil1044@gmail.com>
2023-07-19 12:40:41 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -2,14 +2,14 @@ from flask import Flask, request, jsonify
 from deep_translator import MyMemoryTranslator
 
 app = Flask(__name__)
-translator = MyMemoryTranslator(source='en', target='fr')
+translator = MyMemoryTranslator(source='en-US', target='fr-CA')
 
 def english_to_french(text):
     return translator.translate(text)
 
 def french_to_english(text):
-    translator.source = 'fr'
-    translator.target = 'en'
+    translator.source = 'fr-CA'
+    translator.target = 'en-US'
     return translator.translate(text)
 
 @app.route("/englishToFrench", methods=['POST'])
